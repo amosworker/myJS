4251c0706174f7130aca4feb3d12556b27d7efdf
amosworker <348914779@qq.com>
2014-07-19 16:45:29 +0800

    modify initjs.js

--- myJSBank/initjs.js
+++ myJSBank/initjs.js
@@ -194,11 +194,50 @@ function preDef(evt) {
 
 
 //获取鼠标上下滚轮的距离
+/*
+addEvent(document, 'mousewheel', function (evt) { //非火狐
+	alert(getWD(evt));
+});
+addEvent(document, 'DOMMouseScroll', function (evt) { //火狐
+	alert(getWD(evt));
+});
+*/
 function getMWS(evt) {
 	var e = evt || window.event;
 	if (e.wheelDelta) {
 		return e.wheelDelta;
 	} else if (e.detail) {
-		return -evt.detail * 30; //保持计算的统一
+		return -evt.detail * 40; //保持计算的统一
 	}
 }
+
+
+
+/** 
+ * @brief 获取元素的计算样式（最终的样式） 
+ * @author kenshinlin 2011/11/30 
+ * @param elem 要计算样式的元素,dom对象或字符串（id号） 
+ * @pro 要获取的样式属性,这个字符串是骆驼型的，如marginLeft而不是margin-left 
+ */  
+function getStyle(elem,pro){    
+    elem = ('string'==typeof elem)?document.getElementById('elem'):elem;  
+    if(!elem) return null;  
+      if(elem.style[pro])  //内联  
+          return elem.style[pro];    
+      else if(elem.currentStyle)  //IE  
+      {    
+          return elem.currentStyle[pro];      
+      }   
+      else if(window.getComputedStyle){  //W3C标准  
+          var s=window.getComputedStyle(elem,null);  
+          return s[pro];  
+      }  
+      else if(document.defaultView&&document.defaultView.getComputedStyle) //FF,CHROME等  
+      {    
+          pro=pro.replace(/([A-Z])/g,"-$1"); //如marginLeft转为margin-Left  
+          pro=pro.toLowerCase();   //再转为小写margin-left  
+          var s=document.defaultView.getComputedStyle(elem,"");    
+          return s&&s.getPropertyValue(pro);    
+      }    
+      else return null;  
+} 
