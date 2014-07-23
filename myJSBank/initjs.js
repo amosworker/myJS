79464efa9b31b0c1c327aff528ac430f9b7b0113
amosworker <348914779@qq.com>
2014-07-23 17:47:54 +0800

    add function preloadimages in initjs.js

--- myJSBank/initjs.js
+++ myJSBank/initjs.js
@@ -241,3 +241,40 @@ function getStyle(elem,pro){
       }    
       else return null;  
 } 
+
+/**
+	Javascript实现图片的预加载的完整实现
+	example:
+		preloadimages(["1.jpg","2.jpg"]).done(function(imgarr){
+			alert(imgarr.length);
+			alert(imgarr[0].width);
+		});
+*/
+function preloadimages(arr){   
+	var newimages=[], loadedimages=0;
+	var postaction=function(){};  //此处增加了一个postaction函数
+	var arr=(typeof arr!="object")? [arr] : arr;
+	function imageloadpost(){
+		loadedimages++;
+		if (loadedimages==arr.length){
+			postaction(newimages); 
+			//加载完成用我们调用postaction函数并将newimages数组做为参数传递进去
+		}
+	}
+	for (var i=0; i<arr.length; i++){
+		newimages[i]=new Image();
+		newimages[i].src=arr[i];
+		newimages[i].onload=function(){
+			imageloadpost();
+		};
+		newimages[i].onerror=function(){
+			imageloadpost();
+		};
+	}
+	
+	return { //此处返回一个空白对象的done方法
+		done:function(f){
+			postaction=f || postaction;
+		}
+	}
+}
