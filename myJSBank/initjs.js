1aa126d817596a0c940b708b888606ad48135207
amosworker <348914779@qq.com>
2015-04-14 08:43:53 +0800

    Update initjs.js
    update imageReady

--- myJSBank/initjs.js
+++ myJSBank/initjs.js
@@ -251,41 +251,101 @@ function getStyle(elem,pro){
 } 
 
 /**
-	Javascript实现图片的预加载的完整实现
-	example:
-		preloadimages(["1.jpg","2.jpg"]).done(function(imgarr){
-			alert(imgarr.length);
-			alert(imgarr[0].width);
-		});
-*/
-function preloadimages(arr){   
-	var newimages=[], loadedimages=0;
-	var postaction=function(){};  //此处增加了一个postaction函数
-	var arr=(typeof arr!="object")? [arr] : arr;
-	function imageloadpost(){
-		loadedimages++;
-		if (loadedimages==arr.length){
-			postaction(newimages); 
-			//加载完成用我们调用postaction函数并将newimages数组做为参数传递进去
-		}
-	}
-	for (var i=0; i<arr.length; i++){
-		newimages[i]=new Image();
-		newimages[i].src=arr[i];
-		newimages[i].onload=function(){
-			imageloadpost();
-		};
-		newimages[i].onerror=function(){
-			imageloadpost();
-		};
-	}
-	
-	return { //此处返回一个空白对象的done方法
-		done:function(f){
-			postaction=f || postaction;
-		}
-	}
-}
+     * 图片头数据加载就绪事件 - 更快获取图片尺寸
+     *
+     * 原理：没有缓存的情况下，用计时器不断验证图片的大小是否发生变化，如果不在变化则为ready
+     *       如果有缓存则w3c浏览器会调用 complete，ie则会走 onload，都不在走 计时器那部分
+     *
+     * @version    2011.05.27
+     * @author    TangBin
+     * @see        http://www.planeart.cn/?p=1121
+     * @param    {String}    图片路径
+     * @param    {Function}    尺寸就绪
+     * @param    {Function}    加载完毕 (可选)
+     * @param    {Function}    加载错误 (可选)
+     * @example imgReady('http://www.google.com.hk/intl/zh-CN/images/logo_cn.png', function () {
+alert('size ready: width=' + this.width + '; height=' + this.height);
+});
+     */
+    var imgReady = (function () {
+        var list = [], intervalId = null,
+
+            // 用来执行队列
+            tick = function () {
+                var i = 0;
+                for (; i < list.length; i++) {
+                    list[i].end ? list.splice(i--, 1) : list[i]();
+                }
+                ;
+                !list.length && stop();
+            },
+
+            // 停止所有定时器队列
+            stop = function () {
+                clearInterval(intervalId);
+                intervalId = null;
+            };
+
+        return function (url, ready, load, error) {
+            var onready, width, height, newWidth, newHeight,
+                img = new Image();
+
+            img.src = url;
+
+            // 如果图片被缓存，则直接返回缓存数据
+            if (img.complete) {
+                ready.call(img);
+                load && load.call(img);
+                return;
+            }
+            ;
+
+            width = img.width;
+            height = img.height;
+
+            // 加载错误后的事件
+            img.onerror = function () {
+                error && error.call(img);
+                onready.end = true;
+                img = img.onload = img.onerror = null;
+            };
+
+            // 图片尺寸就绪
+            onready = function () {
+                newWidth = img.width;
+                newHeight = img.height;
+                if (newWidth !== width || newHeight !== height ||
+                    // 如果图片已经在其他地方加载可使用面积检测
+                    newWidth * newHeight > 1024
+                    ) {
+                    ready.call(img);
+                    onready.end = true;
+                }
+                ;
+            };
+            onready();
+
+            // 完全加载完毕的事件
+            img.onload = function () {
+                 // onload在定时器时间差范围内可能比onready快
+                // 这里进行检查并保证onready优先执行
+                !onready.end && onready();
+
+                load && load.call(img);
+
+                // IE gif动画会循环执行onload，置空onload即可
+                img = img.onload = img.onerror = null;
+            };
+
+                // 加入队列中定期执行
+            if (!onready.end) {
+                list.push(onready);
+                // 无论何时只允许出现一个定时器，减少浏览器性能损耗
+                if (intervalId === null) intervalId = setInterval(tick, 40);
+            }
+            ;
+        };
+    })();
 
 //点击回到顶部 为obj对象赋予点击回到顶部事件
 function returnToTop(obj)
